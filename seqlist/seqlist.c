1328dc7081c41113c236d486b9478f5bad40aae5
Rong <rjfsg123@163.com>
2016-02-06 18:31:12 +0800

    SeqList
    o Add destroy/clear/length/capacity/insert/get/delete functions

--- seqlist/seqlist.c
+++ seqlist/seqlist.c
@@ -8,7 +8,7 @@ typedef struct _tag_SeqList
 {
 	int capacity;
 	int length;
-	TSeqListNode *Node;
+	TSeqListNode *node;
 }TSeqList;
 
 SeqList *SeqList_Create(int capacity)
@@ -24,7 +24,107 @@ SeqList *SeqList_Create(int capacity)
 	{
 		ret->capacity = capacity;
 		ret->length = 0;
-		ret->Node = (TSeqListNode *)(ret +1);
+		ret->node = (TSeqListNode *)(ret +1);
+	}
+
+	return ret;
+}
+
+void SeqList_Destroy(SeqList *list)
+{
+	free(list);
+}
+
+void SeqList_Clear(SeqList *list)
+{
+	TSeqList *sList = (TSeqList *)list;
+
+	if(sList != NULL)
+	{
+		sList->length = 0;
+	}
+}
+
+int SeqList_Length(SeqList *list)
+{
+	TSeqList *sList = (TSeqList *)list;
+	int ret = -1;
+
+	if(sList != NULL)
+	{
+		ret = sList->length;
+	}
+
+	return ret;
+}
+
+int SeqList_Capacity(SeqList *list)
+{
+	TSeqList *sList = (TSeqList *)list;
+	int ret = -1;
+
+	if(sList != NULL)
+	{
+		ret = sList->capacity;
+	}
+
+	return ret;
+}
+
+int SeqList_Insert(SeqList *list,SeqListNode* node, int pos)
+{
+	TSeqList *sList = (TSeqList *)list;
+	int ret = (sList != NULL);
+	int i = 0;
+
+	ret = ret && (sList->length+1 <= sList->capacity);
+	ret = ret && (0 <= pos);
+
+	if(ret)
+	{
+		if(pos >= sList->length)
+		{
+			pos = sList->length;
+		}
+
+		for(i = sList->length; i > pos; i--)
+		{
+			sList->node[i] = sList->node[i-1];
+		}
+
+		sList->node[i] = (TSeqListNode)node;
+		sList->length++;
+	}
+
+	return ret;
+}
+
+SeqListNode *SeqList_Get(SeqList *list, int pos)
+{
+	TSeqList *sList = (TSeqList *)list;
+	SeqListNode * ret = NULL;
+
+	if((sList != NULL) && (0 <= pos) && (pos < sList->length))
+	{
+		ret = (SeqListNode *)(sList->node[pos]);
+	}
+	
+	return ret;
+}
+
+SeqListNode *SeqList_Delete(SeqList *list, int pos)
+{
+	TSeqList *sList = (TSeqList *)list;
+	SeqListNode *ret = SeqList_Get(list,pos);
+	int i = 0;
+
+	if(ret != NULL)
+	{
+		for(i = pos+1; i < sList->length; i++)
+		{
+			sList->node[i-1] = sList->node[i];
+		}
+		sList->length--;
 	}
 
 	return ret;
