02f2e31aa6bc9caa916a27a021392078b908ee6d
Rong <rjfsg123@163.com>
2016-02-07 18:14:59 +0800

    LinkList
    o Mkdir linklist
    o Add linklist.c linklist.h

---
+++ linklist/linklist.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "LinkList.h"
+
+
+LinkList *LinkList_Create()
+{
+}
+
+void LinkList_Destroy(LinkList *list)
+{
+}
+
+void LinkList_Clear(LinkList *list)
+{
+}
+
+int LinkList_Length(LinkList *list)
+{
+}
+
+int LinkList_Insert(LinkList *list, LinkListNode *node, int pos)
+{
+}
+
+LinkListNode *LinkList_Get(LinkList *list, int pos)
+{
+}
+
+LinkListNode *LinkList_Delete(LinkList *list, int pos)
+{
+}
+
