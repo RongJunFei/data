895896543edf923315094b9d00e5f213577b072b
Rong <rjfsg123@163.com>
2016-02-07 19:24:11 +0800

    LinkList
    o Add LinkList_Destroy/Clear/Length/Insert/Get/Delete

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -22,25 +22,99 @@ LinkList *LinkList_Create()
 
 void LinkList_Destroy(LinkList *list)
 {
+	free(list);
 }
 
 void LinkList_Clear(LinkList *list)
 {
+	TLinkList *slist = (TLinkList *)list;
+	
+	if(slist != NULL)
+	{
+		slist->length = 0;
+		slist->header.next = NULL;
+	}
 }
 
 int LinkList_Length(LinkList *list)
 {
+	TLinkList * slist = (TLinkList *)list;
+	int ret = -1;
+
+	if(slist != NULL)
+	{
+		ret = slist->length;
+	}
+
+	return ret;
 }
 
 int LinkList_Insert(LinkList *list, LinkListNode *node, int pos)
 {
+	TLinkList *slist = (TLinkList *)list;
+	int ret = (slist != NULL) && (pos >= 0) && (node != NULL);
+	int i = 0;
+
+	if(ret)
+	{
+		LinkListNode *current = (LinkListNode *)slist;
+
+		for(i = 0; (i < pos) && (current->next != NULL); i++)
+		{
+			current = current->next;
+		}
+
+		node->next = current->next;
+		current->next = node;
+
+		slist->length++;
+	}
+
+	return ret;
 }
 
 LinkListNode *LinkList_Get(LinkList *list, int pos)
 {
+	TLinkList *slist = (TLinkList *)list;
+	LinkListNode * ret = NULL;
+	int i = 0;
+
+	if((slist != NULL) && (0 <= pos) && (pos < slist->length))
+	{
+		LinkListNode * current = (LinkListNode *)slist;
+
+		for(i = 0; i < pos; i++)
+		{
+			current = current->next;
+		}
+
+		ret = current->next;
+	}
+
+	return ret;
 }
 
 LinkListNode *LinkList_Delete(LinkList *list, int pos)
 {
+	TLinkList *slist = (TLinkList *)list;
+	LinkListNode * ret = NULL;
+	int i = 0;
+
+	if((slist != NULL) && (0 <= pos) && (pos < slist->length))
+	{
+		LinkListNode * current = (LinkListNode *)slist;
+
+		for(i = 0; i < pos; i++)
+		{
+			current = current->next;
+		}
+
+		ret = current->next;
+		current->next = ret->next;
+
+		slist->length--;
+	}
+
+	return ret;
 }
 
