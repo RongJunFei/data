104ef7e46bdc261153dfbc89723f0aa0fde07c96
Rong <rjfsg123@163.com>
2016-02-07 18:39:53 +0800

    LinkList
    o Add main.c
    o write LinkList_Create

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <malloc.h>
-#include "LinkList.h"
+#include "linklist.h"
 
+typedef struct _tag_LinkList
+{
+	LinkListNode header;
+	int length;
+}TLinkList;
 
 LinkList *LinkList_Create()
 {
+	TLinkList * ret = (TLinkList *)malloc(sizeof(TLinkList));
+	if(ret != NULL)
+	{
+		ret->length = 0;
+		ret->header.next = NULL;
+	}
+
+	return ret;
 }
 
 void LinkList_Destroy(LinkList *list)
---
+++ linklist/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linklist.h"
+
+int main(int argc, const char *argv[])
+{
+	
+	return 0;
+}
