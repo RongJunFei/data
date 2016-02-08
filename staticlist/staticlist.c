0c19b9c0ea8f61ec39ebbb495aa9819889e86448
Rong <rjfsg123@163.com>
2016-02-08 14:11:04 +0800

    StaticList
    o proggram fram

---
+++ staticlist/staticlist.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "staticlist.h"
+
+
+StaticList *StaticList_Create(int capacity)
+{
+}
+
+void StaticList_Destroy(StaticList *list)
+{
+}
+
+void StaticList_Clear(StaticList *list)
+{
+}
+
+int StaticList_Length(StaticList *list)
+{
+}
+
+int StaticList_Capacity(StaticList *list)
+{
+}
+
+int StaticList_Insert(StaticList *list, StaticListNode *node, int pos)
+{
+}
+
+StaticListNode *StaticList_Get(StaticList *list, int pos)
+{
+}
+
+StaticListNode *StaticList_Delete(StaticList *list, int pos)
+{
+}
+
