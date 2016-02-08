651558f6d789490833c32e5ef88598685925a13b
Rong <rjfsg123@163.com>
2016-02-08 20:45:57 +0800

    StaticList
    o Write Insert/Get/Delete

--- staticlist/staticlist.c
+++ staticlist/staticlist.c
@@ -33,7 +33,7 @@ StaticList *StaticList_Create(int capacity)
 		ret->header.data = 0;
 		ret->header.next = 0;
 
-		for(i = 0; i <= capacity; i++)
+		for(i = 1; i <= capacity; i++)
 		{
 			ret->node[i].next = AVAILABLE;
 		}
@@ -92,13 +92,96 @@ int StaticList_Capacity(StaticList *list)
 
 int StaticList_Insert(StaticList *list, StaticListNode *node, int pos)
 {
+	TStaticList *sList = (TStaticList *)list;
+	int ret = (sList != NULL);
+	int current = 0;
+	int index = 0;
+	int i = 0;
+
+	ret = ret && (sList->header.data + 1 <= sList->capacity);
+	ret = ret && (pos >= 0) && (node != NULL);
+
+	if(ret)
+	{
+		for(i = 1; i <= sList->capacity; i++)
+		{
+			if(sList->node[i].next == AVAILABLE)
+			{
+				index = i;
+				break;
+			}
+		}
+
+		sList->node[index].data = (unsigned int)node;
+		sList->node[0] = sList->header;
+
+		for(i = 0; (i < pos)&& (sList->node[current].next != 0); i++)
+		{
+			current = sList->node[current].next;
+		}
+
+		sList->node[index].next = sList->node[current].next;
+		sList->node[current].next = index;
+
+		sList->node[0].data++;
+		sList->header = sList->node[0];
+	}
+
+	return ret;
 }
 
 StaticListNode *StaticList_Get(StaticList *list, int pos)
 {
+	TStaticList *sList = (TStaticList *)list;
+	StaticListNode *ret = NULL;
+	int current = 0;
+	int object = 0;
+	int i = 0;
+
+	if((sList != NULL) && (0 <= pos) && (pos <= sList->header.data))
+	{
+		sList->node[0] = sList->header;
+
+		for(i = 0; i < pos; i++)
+		{
+			current = sList->node[current].next;
+		}
+
+		object = sList->node[current].next;
+
+		ret = (StaticListNode *)sList->node[object].data;
+	}
+
+	return ret;
 }
 
 StaticListNode *StaticList_Delete(StaticList *list, int pos)
 {
+	TStaticList *sList = (TStaticList *)list;
+	StaticListNode *ret = NULL;
+	int current = 0;
+	int object = 0;
+	int i = 0;
+
+	if((sList != NULL) && (0 <= pos) && (pos <= sList->header.data))
+	{
+		sList->node[0] = sList->header;
+
+		for(i = 0; i < pos; i++)
+		{
+			current = sList->node[current].next;
+		}
+
+		object = sList->node[current].next;
+
+		sList->node[current].next = sList->node[object].next;
+		sList->node[0].data--;
+		sList->header = sList->node[0];
+		sList->node[object].next = AVAILABLE;
+
+		ret = (StaticListNode *)sList->node[object].data;
+	}
+
+	return ret;
 }
 
